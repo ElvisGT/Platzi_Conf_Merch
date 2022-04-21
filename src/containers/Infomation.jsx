7a80c000c60b03cbc6cfa2c937c2858a9243ca83
Elvis <elvisgiltorres@gmail.com>
2022-04-21 19:02:39 -0400

    Listo el componente Information

--- src/containers/Infomation.jsx
+++ src/containers/Infomation.jsx
@@ -4,7 +4,43 @@ import '../styles/components/Information.css';
 
 const Information = () => {
     return (
-        <h1>Information</h1>
+        <div className="Information">
+            <div className="Information-content">
+                <div className="Information-head">
+                    <h2>Informacion de contacto:</h2>
+                </div>
+                <div className="Information-form">
+                    <form action="">
+                        <input type="texto" placeholder="Nombre completo" name="name"></input>
+                        <input type="text" placeholder="Correo electronico" name="email"></input>
+                        <input type="texto" placeholder="Direccion" name="address"></input>
+                        <input type="texto" placeholder="Apto" name="apto"></input>
+                        <input type="texto" placeholder="Ciudad" name="city"></input>
+                        <input type="texto" placeholder="Pais" name="country"></input>
+                        <input type="texto" placeholder="Estado" name="state"></input>
+                        <input type="texto" placeholder="Codigo Postal" name="cp"></input>
+                        <input type="texto" placeholder="Telefono" name="phone"></input>
+                    </form>
+                </div>
+                <div className="Information-buttons">
+                    <div className="Information-back">
+                        Regresar
+                    </div>
+                    <div className="Information-next">
+                        Pagar
+                    </div>
+                </div>
+            </div>
+            <div className="Information-sidebar">
+                <h3>Pedido:</h3>
+                <div className="Information-item">
+                    <div className="Information-element">
+                        <h4>Item name</h4>
+                        <span> $10</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
