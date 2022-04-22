13ab221bd524e1a336168ea7824fb43a573c9440
Elvis <elvisgiltorres@gmail.com>
2022-04-22 17:23:58 -0400

    useRef en Checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -9,11 +9,11 @@ const Checkout = () => {
     const {state,removeFromCart} = useContext(AppContext); 
     const {cart} = state;
 
-    const HandleRemove = product => () => {
+    const handleRemove = product => () => {
         removeFromCart(product);
     }
 
-    const HandleSumTotal = () => {
+    const handleSumTotal = () => {
         const reducer = (accumulator,currentValue) => accumulator + currentValue.price;
         const sum = cart.reduce(reducer,0);
         return sum;
@@ -24,12 +24,12 @@ const Checkout = () => {
             <div className="Checkout-content">
                 {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos...</h3>}
                 {cart.map(item => (
-                    <div className="Checkout-item">
+                    <div className="Checkout-item"  key={item.id}>
                         <div className="Checkout-element">
                             <h4>{item.title}</h4>
                             <span>${item.price}</span>
                         </div>
-                        <button type="button" onClick={HandleRemove(item)}><i className="fas fa-trash-alt" /></button>
+                        <button type="button" onClick={handleRemove(item)}><i className="fas fa-trash-alt" /></button>
                     </div>
 
                 ))
@@ -38,7 +38,7 @@ const Checkout = () => {
                 </div>
                 {cart.length > 0 && 
                     <div className="Checkout-sidebar">
-                        <h3>{`Precio total: $ ${HandleSumTotal()}`}</h3>
+                        <h3>{`Precio total: $ ${handleSumTotal()}`}</h3>
                         <Link to="/checkout/information">
                             <button type="button">Continuar pedido</button>
 
